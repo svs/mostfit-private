f7af93d3b3753a548ba18be51266fb16370ad343
Siddharth Sharma <svs@intellecap.net>
2012-02-08 20:10:22 +0530

    Squashed commit of the following:
    commit 8b44fbc9855501f52baf9943b5a42b3233551f13
    Author: Siddharth Sharma <svs@intellecap.net>
    Date:   Tue Feb 7 21:56:24 2012 +0530
    
        Added specs for CenterMeetingDay
    
        Lot of passing tests now.
        Cleaned up the API on CenterMeetingDay and Center.
        Also added spec_helper.spork but can't seem to get it to work.
    
    commit 3752ef519440cbebafb766118b987cf65c050e48
    Author: Siddharth Sharma <svs@intellecap.net>
    Date:   Tue Feb 7 10:44:59 2012 +0530
    
        Fixed CenterMeetingDay
    
        added loads of specs
        cleaned up the API
        added TomDoc to the whole file
    
    commit 68b5ad73135d0a4d61fc5e5c05a98616f5dcf552
    Author: Siddharth Sharma <svs@intellecap.net>
    Date:   Mon Feb 6 12:46:24 2012 +0530
    
        added datevector spec.
        fixed some corner cases in datevector
        added some more specs for center
        introduced first TomDocs into code http://tomdoc.org/
    
    commit 03ea9d8ebf79b881978cc33f12d51e07bc0f766b
    Author: Siddharth Sharma <svs@intellecap.net>
    Date:   Fri Feb 3 02:21:45 2012 +0530
    
        WIP - fixing centers and center_meeting_days once and for all
    
    commit 05153ae7bd7926457da4be60aa50c2b0cbfc3055
    Merge: 9ebffed cd18ec8
    Author: Siddharth Sharma <svs@intellecap.net>
    Date:   Thu Feb 2 17:45:55 2012 +0530
    
        Merge branch 'takeover'

--- app/models/center_meeting_day.rb
+++ app/models/center_meeting_day.rb
@@ -3,13 +3,16 @@ class CenterMeetingDay
   include DataMapper::Resource
   DAYS = [:none, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
 
+  before :valid?, :convert_blank_to_nil
+  before :valid?, :set_date_vector_properties              # the meeting_day property is just for show. internally, we ensure that :what is always set
+# before :valid?, :substitute_nils_for_dates
   after :destroy, :fix_dates
   after :save,    :add_loans_to_queue
 
   property :id, Serial
   property :center_id, Integer, :index => false, :nullable => false
   property :meeting_day, Enum.send('[]', *DAYS), :nullable => false, :default => :none, :index => true
-  property :valid_from,  Date, :nullable => false
+  property :valid_from,  Date, :nullable => true # if nil then this must be the first CMD for the center
   property :valid_upto,  Date, :nullable => true # we do not really need valid upto.  CMDs are valid up to the next center meeting days start from
   
   property :deleted_at,  ParanoidDateTime
@@ -18,51 +21,58 @@ class CenterMeetingDay
   # see lib/date_vector.rb for details but in short one can say things like
   # :every => [2,4], :what => :thursday, :of_every => 2, :period => :month to mean "every 2nd and 4th thursday of every 2nd month"
   # this is the kind of feature that sets Mostfit miles apart from the rest of the pack!
+  # for now we will allow only one datevector type per center. This means a center can only have one meeting schedule frequency
 
   property :every, CommaSeparatedList 
   property :what, Enum.send('[]',*([:day] + DAYS[1..-1]))
   property :of_every, Integer
   property :period, Enum[nil,:week, :month], :nullable => true
 
-  property :new_what, String
+  # end datevector properties
 
-  validates_with_method :either_meeting_day_or_date_vector
-  
+  # RELATIONSHIPS
   belongs_to :center
 
-  before :valid?, :convert_blank_to_nil
-  
-  
-  def either_meeting_day_or_date_vector
-    date_vector_valid = every and (not every.blank?) and what and (not what.blank?) and of_every and (not of_every.blank?) and period and (not period.blank?)
-    return true if meeting_day or date_vector_valid
-    return [false, 'Choose either a meeting day or a scheme to set up a schedule']
-  end
-
-  def to_s
-    "from #{valid_from} to #{valid_upto} : #{meeting_day_string}"
-  end
-
-  after :destroy, :fix_dates
-
-
+  # VALIDATIONS
+  validates_with_method :either_meeting_day_or_date_vector
   validates_with_method :valid_from_is_lesser_than_valid_upto
   validates_with_method :dates_do_not_overlap
   validates_with_method :check_not_last, :if => Proc.new{|t| t.deleted_at}
 
-  def check_not_last
-    return true unless center
-    return true unless deleted_at
-    return [false,"cannot delete the last center meeting date"] if (self.center.center_meeting_days.count == 1 and (self.center.meeting_day == :none or (not self.center.meeting_day)))
-    return true
-  end
-  
-  def last_date
-    valid_upto || Date.new(2100,12,31)
+  # REPRESENTATION
+  # Public: Returns a String representation of the center meeting date
+  def to_s
+    "from #{valid_from} to #{valid_upto} : #{meeting_day_string}"
   end
 
-  # adding the new properties to calculate the datevector for this center.
-  # for now we will allow only one datevector type per center. This means a center can only have one meeting schedule frequency
+  
+  # Public: Returns a String representation of the meeting day
+  def meeting_day_string
+    return meeting_day.to_s if meeting_day and meeting_day != :none
+    "#{every.join(',')} #{what} of every #{of_every} #{period}" rescue meeting_day
+  end
+
+  # Public: Returns the weekday of the meeting day 
+  # 
+  # some complications arise due to the fact that the meeting day can be specified either on the :meeting_day
+  # parameter or even on the :what parameter
+  def meeting_wday
+    return meeting_day if meeting_day and meeting_day != :none
+    return what if what != :day
+    return period if period == :month
+    return meeting_day
+  end
+
+
+  # DATES
+
+  # Public: returns the last Date upto which this CenterMeetingDay is valid
+  def last_date
+    valid_upto || SEP_DATE
+  end
+
+
+  # Public: creates and return a DateVector which matches this CenterMeetingDay schedule
   def date_vector(from = self.valid_from, to = last_date)
     if every and what and of_every and period
       DateVector.new(every, what, of_every, period.to_sym, from, to)
@@ -72,73 +82,60 @@ class CenterMeetingDay
   end
 
   def get_dates(from = self.valid_from, to = last_date)
+    # DEPRECATED use #meeting_dates instead
+    meeting_dates(from, to)
+  end
+
+  # Public: returns an array of Dates representing the meeting dates covered by this CenterMeetingDay
+  def meeting_dates(from = self.valid_from, to = last_date)
     date_vector(from, to).get_dates
   end
 
+  # Public: returns an array of the next 'n' dates for this CenterMeetingDay
+  #
+  # n: an Integer - how many dates you want?
+  # from: an optional Date setting the lower bound of the dates
   def get_next_n_dates(n, from = self.valid_from)
     get_dates(from, n)
   end
 
-  def meeting_day_string
-    return meeting_day.to_s if meeting_day and meeting_day != :none
-    "#{every.join(',')} #{what} of every #{of_every} #{period}" rescue meeting_day
-  end
-
-  def meeting_wday
-    # returns only the weekday of the meeting day
-    return meeting_day if meeting_day and meeting_day != :none
-    return what if what != :day
-    return period if period == :month
-    return meeting_day
-  end
-
-  def to_s
-    "from #{valid_from} to #{valid_upto} : #{meeting_day_string}"
-  end
-
+  # SEARCH
   
-  def valid_from_is_lesser_than_valid_upto
-    self.valid_from = Date.parse(self.valid_from) unless self.valid_from.is_a? Date
-    self.valid_upto = (self.valid_upto.blank? ? Date.new(2100,12,31) : Date.parse(self.valid_upto))     if self.valid_upto.class == String
-
-    if self.valid_from and self.valid_upto
-      return [false, "Valid from date cannot be before than valid upto date"] if self.valid_from > self.valid_upto
-    end
-    return true    
-
+  # Public: returns the center meeting days in force on a particular day for a particular list of centers
+  #
+  # date: the Date for which the list is required
+  # centers: a Hash specifiying the selection or a DataMapper::Collection of centers to filter for
+  # meeting_day: an optional Symbol for meeting_day for which to filter the list
+  #
+  # Examples
+  # CenterMeetingDay.in_force_on(Date.today, Center.all(:id => 1))
+  def self.in_force_on(date, centers = {}, meeting_day = nil)
+    center_ids = centers.is_a?(Hash) ? Center.all(centers).aggregate(:id) : centers.aggregate(:id)
+    raise ArgumentError.new("Strange weekday you got") if meeting_day and (not WEEKDAYS.include?(meeting_day))
+    meeting_day_selection = WEEKDAYS.include?(meeting_day) ? {:what => meeting_day} : {}
+    # first get the CMDs with both valid_from and valid_upto
+    # there will only be one for each center because they cannot overlap
+    c1 = CenterMeetingDay.all(meeting_day_selection.merge(:valid_from.lte => date, :valid_upto.gte => date, :center_id => center_ids))
+    center_ids = center_ids - c1.aggregate(:center_id)
+    # then get the CMDs with valid_From = nil with proper valid_upto which are not in the above array
+    c2 = CenterMeetingDay.all(meeting_day_selection.merge(:valid_from => nil, :valid_upto.gte => date, :center_id => center_ids))
+    center_ids = center_ids - c2.aggregate(:center_id)
+    # now the above can contain multiple lines per center, so deal with that
+    c2 = c2.group_by{|c| c.center_id}.map{|cid, c| c.sort_by{|_c| _c.valid_from}[-1]}
+    # then get the CMDs with valid_From  without proper valid_upto which are not in the above array
+    c3 = CenterMeetingDay.all(meeting_day_selection.merge(:valid_from.lte => date, :valid_upto => nil, :center_id => center_ids))
+    center_ids = center_ids - c3.aggregate(:center_id)
+    # now the above can contain multiple lines per center, so deal with that
+    c3 = c3.group_by{|c| c.center_id}.map{|cid, c| c.sort_by{|_c| _c.valid_from}[-1]}
+    # then get the CMDs with no valid_upto either
+    c4 = CenterMeetingDay.all(meeting_day_selection.merge(:valid_from => nil, :valid_upto => nil, :center_id => center_ids))
+    
+    c1 + c2 + c3 + c4
+    
   end
-
-  # checks that for a given center, the valid_from and valid_to dates for this center do not overlap with another center_meeting_day
-  def dates_do_not_overlap
-    return true if deleted_at
-    return true unless self.center
-    cmds = self.center.center_meeting_days
-    return true if cmds.count == 0
-    return true if cmds.count == 1 and cmds.first.id == self.id
-    bad_ones = center.center_meeting_days.map do |cmd| 
-      if cmd.id == id
-        true
-      else
-        if cmd.valid_upto and cmd.valid_upto != Date.new(2100,12,31) # an end date is specified for the other cmd 
-          if valid_upto  and valid_upto != Date.new(2100,12,31)          # and for ourselves
-            cmd.valid_from > valid_upto or cmd.valid_upto < valid_from # either we end before the other one starts or start after the other one ends
-          else            # but not for ourselves
-            valid_from > cmd.valid_upto or valid_from < cmd.valid_from # either we start after the other one starts or we start after the other one ends
-          end
-        else                 # no end date specified for the other one
-          if valid_upto and  valid_upto != Date.new(2100,12,31)     # but we have one
-            valid_from > cmd.valid_from or valid_upto < cmd.valid_from  # either we start after the other one starts or we end before the other one starts
-          else               # neither one has an end date
-            true
-          end
-        end
-      end
-    end
-    return [false, "Center Meeting Day validity overlaps with another center meeting day."] if bad_ones.select{|x| not x}.count > 0
-    return true
-  end
-
-
+  
+  private
+  
   # adds the loans from this center into the dirty_loan queue to recreate their history
   def add_loans_to_queue
     loan_ids = self.center.loans.aggregate(:id) rescue nil
@@ -148,6 +145,8 @@ class CenterMeetingDay
     DirtyLoan.send(:class_variable_set,"@@poke_thread", true)
   end
 
+
+  # Private: 
   def fix_dates
     cmds = CenterMeetingDay.all(:order => [:valid_from], :center_id => self.center_id)
     cmds.each_with_index{|cmd, idx|
@@ -172,6 +171,12 @@ class CenterMeetingDay
     end
   end
 
+  # Private: substitues nil in the valid_from and valid_upto fields with suitably improbable dates
+  def substitute_nils_for_dates
+    self.valid_from = Date.min_date unless self.valid_from
+    self.valid_upto = SEP_DATE unless self.valid_upto
+  end
+
   def convert_blank_to_nil
     self.attributes.each{|k, v|
       if v.is_a?(String) and v.empty? and [Integer, Enum].include?(self.class.properties.find{|x| x.name == k}.type)
@@ -181,5 +186,80 @@ class CenterMeetingDay
     self.period = self.period.to_sym rescue nil
   end
 
+  # Private: ensures that every CMD has a valid datevector so we can depend only on those properties
+  # for our calculations and representations
+  def set_date_vector_properties
+    if meeting_day and meeting_day != :none
+      self.every = "1"; self.what = self.meeting_day; self.of_every = 1; self.period = :week
+    else
+      self.every = self.what = self.meeting_day = self.of_every = self.period = nil
+    end
+  end
+
+  # VALIDATIONS
+  # Private: checks that the valid from date is less than the valid upto dates, adjusting for nils
+  def valid_from_is_lesser_than_valid_upto
+    return true if self.valid_from.blank? and self.valid_upto.blank? # neither is set
+    self.valid_from = Date.parse(self.valid_from) if self.valid_from.is_a?(String)
+    self.valid_upto = (self.valid_upto.blank? ? SEP_DATE : Date.parse(self.valid_upto))     if self.valid_upto.class == String
+
+    if self.valid_from and self.valid_upto
+      return [false, "Valid from date cannot be before than valid upto date"] if self.valid_from > self.valid_upto
+    end
+    return true    
+  end
+
+  # Private: checks that for a given center, the valid_from and valid_to dates for this center do not overlap with another center_meeting_day
+  def dates_do_not_overlap
+    return true if deleted_at
+    return true unless self.center
+    cmds = self.center.center_meeting_days
+    return true if cmds.count == 0
+    if cmds.count == 1
+      return true  if cmds.first.id == self.id
+      if cmds.first.valid_from == nil
+        return true if self.valid_from
+        return [false, "This Center Meeting Schedule definition must have a valid_from date set"]
+      end
+    end
+    bad_ones = center.center_meeting_days.map do |cmd| 
+      if cmd.id == id
+        true
+      else
+        if cmd.valid_upto and cmd.valid_upto != SEP_DATE                                                             # an end date is specified for the other cmd 
+          if valid_upto  and valid_upto != SEP_DATE                                                                  # and for ourselves
+            valid_from ? (cmd.valid_from > valid_upto or  cmd.valid_upto < valid_from) : cmd.valid_from > valid_upto # either we end before the other one starts or start after the other one ends
+          else                                                                                                       # but not for ourselves
+            valid_from > cmd.valid_upto or valid_from < cmd.valid_from                                               # either we start after the other one starts or we start after the other one ends
+          end
+        else                                                                                                         # no end date specified for the other one
+          if valid_upto and  valid_upto != SEP_DATE                                                                  # but we have one
+            cmd.valid_from ? (valid_from > cmd.valid_from or valid_upto < cmd.valid_from) : true                     # either we start after the other one starts or we end before the other one starts
+          else                                                                                                       # neither one has an end date
+            true
+          end
+        end
+      end
+    end
+    return [false, "Center Meeting Day validity overlaps with another center meeting day."] if bad_ones.select{|x| not x}.count > 0
+    return true
+  end
+
+
+  # Private: checks that user has specified either a meeting_day or a valid datevector
+  def either_meeting_day_or_date_vector
+    date_vector_valid = every and (not every.blank?) and what and (not what.blank?) and of_every and (not of_every.blank?) and period and (not period.blank?)
+    return true if meeting_day or date_vector_valid
+    return [false, 'Choose either a meeting day or a scheme to set up a schedule']
+  end
+
+  # Private: ensures that one cannot delete the last center_meeting_day for a center
+  def check_not_last
+    return true unless center
+    return true unless deleted_at
+    return [false,"cannot delete the last center meeting date"] if (self.center.center_meeting_days.count == 1 and (self.center.meeting_day == :none or (not self.center.meeting_day)))
+    return true
+  end
+  
 
 end
---
+++ spec/models/center_meeting_day_spec.rb
@@ -0,0 +1,134 @@
+require File.join( File.dirname(__FILE__), '..', "spec_helper" )
+
+describe CenterMeetingDay do
+
+  before(:all) do
+    @manager = Factory(:staff_member)
+    @manager.should be_valid
+
+    @user = Factory(:user)
+    @user.should be_valid
+
+    @branch = Factory(:branch, :manager => @manager)
+    @branch.should be_valid
+
+  end
+
+  before(:each) do
+    repository.adapter.execute("truncate table centers") # truncate resets the ids as well. this ensures that the catalog test passescata
+    CenterMeetingDay.all.destroy!
+    @center = Center.new(:name => "Munnar hill center")
+    @center.manager = @manager
+    @center.branch = @branch
+    @center.creation_date = Date.new(2010, 1, 1)
+    @center.meeting_day = :monday
+    @center.code = "center"
+    @center_meeting_day = CenterMeetingDay.new(:meeting_day => :thursday)
+    @center.center_meeting_days << @center_meeting_day
+    @center.save
+    @center.should be_valid
+    @center.errors.each{|e| puts e}
+  end
+
+  it "should be valid with a meeting day" do
+    @center_meeting_day.valid?
+    @center_meeting_day.errors.each{|e| puts e}
+    @center_meeting_day.should be_valid
+
+    @center_meeting_day.meeting_day = nil
+    @center_meeting_day.should_not be_valid
+  end
+
+  it "should work correctly when added to a center with existing center_meeting_days" do
+    @cmd2 = CenterMeetingDay.new(:meeting_day => :friday, :center => @center)
+    @cmd2.should_not be_valid # because the center already has one CMD without valid_from and valid_upto
+
+    @cmd2.valid_from = Date.today
+    @cmd2.should be_valid
+
+    @cmd2.valid_from = nil
+    @cmd2.valid_upto = Date.today
+    @cmd2.should_not be_valid  # only the first CMD can have a nil valid_from
+    # actually this is something to be debated
+    # the way I see it, we can have the following situation
+    # first CMD     valid_from => nil, valid_upto => nil   # call this 'default' CMD
+    # after that    valid_from => nil, valid_upto => d1    # i.e. this is in vogue from start until d1 and after that 'default' CMD
+    # or            valid_upto => nil, valid_from => d2    # i.e. upto d2 default, then this one
+    # if both are specified then, given d1 > d2, we cannot calculate a schedule so we cannot allow this scheme
+    #                             given d2 > d1 we get d1, d2 and default is never used.....
+    # or is this too confusing?
+    # in the current scheme, as many CMDs can have nul valid_upto but only one CMD can have nil valid_from
+    # which makes everything much simpler in my opinion
+
+  end
+
+  it "should check that center meeting dates do not overlap" do
+    # should not be valid wthout valid_from
+    @cmd2 = CenterMeetingDay.new(:meeting_day => :friday, :center => @center)
+    @cmd2.should_not be_valid
+    # should be valid with a valid_From and without valid_upto
+    @cmd2 = CenterMeetingDay.new(:meeting_day => :friday, :center => @center, :valid_from => Date.today)
+    @cmd2.should be_valid
+    @cmd2 = CenterMeetingDay.new(:meeting_day => :friday, :center => @center, :valid_from => Date.today, :valid_upto => Date.today + 30)
+    @cmd2.should be_valid
+    @cmd2.save
+    # reload the center and try with an overlapping date
+    @center = Center.get(@center.id)
+    @cmd3 = CenterMeetingDay.new(:meeting_day => :friday, :center => @center, :valid_from => Date.today + 15, :valid_upto => Date.today + 45)
+    @cmd3.should_not be_valid
+    # check the corner case
+    @cmd3.valid_from = Date.today + 30
+    @cmd3.should_not be_valid
+    # now check with valid interval
+    @cmd3.valid_from = Date.today + 31
+    @cmd3.should be_valid
+    @cmd3.save
+    # try deleting a center meeting day
+    @cmd3.destroy
+    @center = Center.get(@center.id)
+    @center.center_meeting_days.count.should == 2
+  end
+
+  it "should return correct centers meeting days in force on a given date" do
+    repository.adapter.execute("truncate table centers") # truncate resets the ids as well. this ensures that the catalog test passescata
+    repository.adapter.execute("truncate table center_meeting_days") # truncate resets the ids as well. this ensures that the catalog test passescata
+
+    @branch_1 = Factory(:branch, :name => "Branch 1", :manager => @manager)
+    @branch_2 = Factory(:branch, :name => "Branch 2", :manager => @manager)
+    @b1c1 = Center.create(:name => "b1c1", :branch => @branch_1, :meeting_day => :monday, :manager => @manager, :code => "a")
+    @b1c2 = Center.create(:name => "b1c2", :branch => @branch_1, :meeting_day => :tuesday, :manager => @manager, :code => "b")
+    @b2c1 = Center.create(:name => "b2c1", :branch => @branch_2, :meeting_day => :tuesday, :manager => @manager, :code => "c")
+    @b2c2 = Center.create(:name => "b2c2", :branch => @branch_2, :meeting_day => :thursday, :manager => @manager, :code => "d")
+    
+    centers = Center.all
+
+    CenterMeetingDay.in_force_on(Date.today, centers).should == CenterMeetingDay.all
+    
+    @cmd = CenterMeetingDay.new(:center => @b1c1, :valid_from => Date.today, :meeting_day => :saturday)
+    @cmd.should be_valid
+    @cmd.save
+    
+    CenterMeetingDay.in_force_on(Date.today - 1, centers).should == CenterMeetingDay.all.to_a[0..-2]
+    CenterMeetingDay.in_force_on(Date.today, centers).map(&:id).sort.should == CenterMeetingDay.all(:order => [:id]).to_a[1..-1].map(&:id).sort
+
+    CenterMeetingDay.in_force_on(Date.today, centers, :tuesday).map(&:center_id).sort.should == [2,3]
+
+    # now check what happens if we ask for in_force_on for a date beyond the last valid_upto date.
+    # expected behaviour - if the center has CMD with valid_upto => nil, then that one will come back into effect after the validity date
+    @cmd.valid_upto = Date.today + 10
+    @cmd.should be_valid
+    @cmd.save
+    CenterMeetingDay.in_force_on(Date.today + 11, centers).map(&:id).sort.should == CenterMeetingDay.all(:order => [:id]).to_a[0..-2].map(&:id).sort
+
+    @ocmd = CenterMeetingDay.first
+    @ocmd.valid_upto = Date.today
+    @ocmd.should_not be_valid # overlap with @cmd
+
+    @ocmd.valid_upto = Date.today - 1
+    @ocmd.should be_valid
+    @ocmd.save
+    CenterMeetingDay.in_force_on(Date.today + 11, centers).map(&:id).sort.should == CenterMeetingDay.all(:order => [:id]).to_a[1..-2].map(&:id).sort
+    
+  end
+
+end
