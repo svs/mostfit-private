d8e263496f348c390c45454e69e185def9da66c4
Siddharth Sharma <svs@svs.io>
2012-06-05 16:34:21 +0530

    Revert "repayment overdue report"
    This reverts commit 85eb751eb1abf3f5383e2c2dad20f4e38b6335cb.

--- app/models/reports/repayment_overdue.rb
+++ app/models/reports/repayment_overdue.rb
@@ -1,8 +1,7 @@
 class RepaymentOverdue < Report
   attr_accessor :date, :branch, :center, :branch_id, :center_id, :staff_member_id, :loan_product_id, :funder_id
 
-  validates_with_method :is_valid
-
+  validates_with_method :branch_id, :branch_should_be_selected  
 
   def initialize(params, dates, user)
     @date   = (dates and dates[:date]) ? dates[:date] : Date.today
@@ -18,37 +17,95 @@ class RepaymentOverdue < Report
     "Repayment overdue register as  of #{@date}"
   end
 
-  def is_valid
-    return [false, "Please choose a staff member and a date"] unless (@staff_member_id and @date)
-    return true
-  end
-
   def generate
-    s = StaffMember.get(@staff_member_id)
-    filter = s ? {:loan_history__center_id => s.centers.aggregate(:id)} : (@center_id ? {:loan_history__center_id => @center} : {})
-    filter.merge!(@branch_id ? {:branch_id => @branch_id} : {})
-    filter.merge!(:date => @date)
+    data, clients, loans, centers, branches, payments, hash = {}, {}, {}, {}, {}, {:principal => {}, :interest => {}, :fees => {}}, {}
+    hash[:branch_id] = @branch.map{|x| x.id}
+    hash[:center_id] = @center.map{|x| x.id}
+    hash[:loan_product_id] = self.loan_product_id  if self.loan_product_id
 
-    q = DB.from(:loan_history, :clients, :centers, :branches, :loans)
-    q = q.where(:loan_history__loan_id   => :loans__id,
-                :loan_history__client_id => :clients__id, 
-                :loan_history__center_id => :centers__id,
-                :loan_history__branch_id => :branches__id).filter(filter)
-    debugger
-    q = q.filter{(interest_due + principal_due + fees_due_today) > 0}
-    q = q.select(:branches__name  => :branch, 
-                 :branches__id     => :branch_id,
-                 :centers__name   => :center,
-                 :centers__id     => :center_id,
-                 :clients__name   => :client, 
-                 :clients__id     => :client_id,
-                 :loans__id       => :loan_id,
-                 :interest_due    => :interest_due, 
-                 :principal_due   => :principal_due,
-                 :fees_due_today  => :fees_due,
-                 :date            => :date)
-    q.all
+    # if a funder is selected
+    funder_loan_ids = @funder.loan_ids if @funder
+    hash[:id]       = funder_loan_ids if @funder
+    histories = LoanHistory.defaulted_loan_info_by(:loan, @date, hash, ["branch_id", "center_id", "client_id", "actual_outstanding_principal"])
 
+    if histories and histories.length > 0
+      client_ids = histories.map{|x| x.client_id}
+      clients  = Client.all(:id => client_ids, :fields => [:id, :name]).map{|x| [x.id, x]}.to_hash
+      loans    = Loan.all(:client_id => client_ids, :fields => [:id, :client_id, :amount]).aggregate(:id, :amount).to_hash
+      payments[:principal] = Payment.all(:loan_id => loans.keys, :type => :principal, :received_on.lte => @date).aggregate(:loan_id, :amount.sum).to_hash
+      payments[:interest]  = Payment.all(:loan_id => loans.keys, :type => :interest, :received_on.lte => @date).aggregate(:loan_id, :amount.sum).to_hash
+      payments[:fees]      = Payment.all(:loan_id => loans.keys, :type => :fees, :received_on.lte => @date).aggregate(:loan_id, :amount.sum).to_hash
+    end
+    
+    histories = histories.group_by{|x| x.center_id}
+
+    # get all the loan dues
+    @branch.each{|b|
+      data[b] = {}
+      branches[b.id] = b
+      b.centers.each{|c|
+        centers[c.id] = c
+        next if @center and not @center.include?(c)
+        data[b][c] ||= {}
+        histories[c.id].each{|row|
+          data[b][c][clients[row.client_id]] ||= []
+          prin  = payments[:principal][row.loan_id]||0
+          int   = payments[:interest][row.loan_id]||0
+          fee   = payments[:fees][row.loan_id]||0
+          total = prin + int + fee
+          data[b][c][clients[row.client_id]] << [row.loan_id, loans[row.loan_id], prin, int, fee, total,                                                 
+                                                 loans[row.loan_id] - prin,
+                                                 row.pdiff, row.tdiff-row.pdiff, 0]
+        } if histories[c.id]
+      }
+    }
+
+    # all the fee dues
+    fees_due         = Fee.overdue(@date)
+    fees_due_keys    = fees_due.keys & funder_loan_ids if @funder
+    fees_due_loans   = fees_due.length > 0 ? Loan.all(:id => fees_due_keys, :fields => [:id, :client_id, :amount]) : []
+    fees_due_clients = Client.all(:id => fees_due_loans.map{|x| x.client_id}, :fields => [:id, :name, :center_id]).map{|c| [c.id, c]}.to_hash
+    
+    fees_due.each{|loan_id, amount|
+      loan   = fees_due_loans.find{|x| x.id==loan_id}
+      next unless loan
+      client = fees_due_clients[loan.client_id]
+      center = centers[client.center_id]
+      next unless center
+      branch = branches[center.branch_id]
+      if data[branch][center][client] and row = data[branch][center][client].find{|x| x[0] == loan_id}
+        row[9]+=amount
+      else
+        data[branch][center][client] = [[loan_id, loan.amount, 0, 0, 0, 0, loan.amount, 0, 0, amount]]
+      end
+    }
+
+    # client fee dues
+    Fee.all(:payable_on => [:client_date_joined, :client_grt_pass_date]).each{|fee| 
+      client_paid    = Payment.all(:type => :fees, :fee => fee, :amount => fee.amount, :received_on.lte => @date).aggregate(:client_id)
+      
+      hash           = {:client_type => fee.client_types, :fields => [:id], :center => @center}
+      hash[:id]      = Loan.all(:fields => [:id, :client_id], :id => funder_loan_ids).map{|x| x.client_id} if @funder
+
+      # set appropriate filter for client GRT pass date or date joined depending on the fee
+      fee.payable_on == :client_grt_pass_date ? hash[:grt_pass_date.lte] = @date : hash[:date_joined.lte] = @date 
+
+      client_payable = Client.all(hash).map{|x| x.id}
+
+      # get clients which have a payable but have not paid
+      clients        = Client.all(:id => (client_payable - client_paid), :fields => [:id, :name, :center_id])      
+      clients.each{|client|
+        center = centers[client.center_id]
+        next unless center
+        branch = branches[center.branch_id]
+        if data[branch][center][client] and row = data[branch][center][client].first
+          row[9]+=fee.amount
+        else
+          data[branch][center][client] = [[0, 0, 0, 0, 0, 0, 0, 0, 0, fee.amount]]
+        end
+      }
+    }
+
+    return data
   end
-
 end
